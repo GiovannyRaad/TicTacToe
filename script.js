c85feec398ab8251210cf303ebc38dead4836dbe
GiovannyRaad <213656413+GiovannyRaad@users.noreply.github.com>
2025-08-01 21:54:21 +0300

    Add win line with js logic for winning
    Line gets drawn on the win and clicks get blocked after the win

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 let grid = new Array(9).fill(9); /* creates and fills the game board with 9s */
 let turn = 1; /* 1 = x and 0 = O */
+let winner;
+let running = true;
 
 const cells = document.querySelectorAll(".cell");
+const winCon = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [2, 4, 6],
+  [2, 5, 8],
+  [6, 7, 8],
+  [1, 4, 7],
+  [3, 4, 5]
+];
+const line = document.querySelector(".line");
 
 let prev_cell;
+
 
 cells.forEach(cell => {
     cell.addEventListener("click", handleClick);
 });
+
+
 
 function resetFlicker(){ /* stops the flicker animation of the previous cell */
     if (prev_cell){
@@ -24,6 +40,7 @@ function handleClick(event){
         grid[index] = turn;
         draw(cell, turn);
         turn = (turn + 1) % 2;
+        winCheck();
 
     }
 }
@@ -47,4 +64,75 @@ function draw(cell, turn){
     span.style.fontSize = "1em";
     
 
+}
+
+function winCheck(){
+    let pattern = 1;
+    for (const con of winCon){
+        const result = grid[con[0]] + grid[con[1]] + grid[con[2]]; /* if = 0, O wins, if = 3, X wins */
+        if (result == 0){
+            winner = 0;
+            drawWinLine(pattern);
+            endGame();
+        } else if (result == 3){
+            winner = 1;
+            drawWinLine(pattern);
+            endGame();
+        }
+        pattern++;
+    }
+}
+
+function drawWinLine(pattern){ /* change rotation and location of line */
+    switch (pattern) {
+        
+        case 1:
+            line.style.rotate = "90deg";
+            line.style.bottom = "68%";
+            line.style.height = "90%";
+            break;
+
+        case 2:
+            line.style.rotate = "0deg";
+            line.style.right = "66.5%";
+            line.style.height = "90%";
+            break;
+        
+        case 3:
+            line.style.rotate = "-45deg";
+            line.style.height = "110%";
+            break;
+
+        case 4:
+            line.style.rotate = "45deg";
+            line.style.height = "110%";
+            break;
+
+        case 5:
+            line.style.rotate = "0deg";
+            line.style.left = "66.5%";
+            line.style.height = "90%";
+            break;
+
+        case 6:
+            line.style.rotate = "90deg";
+            line.style.top = "68%";
+            line.style.height = "90%";
+            break;
+
+        case 7:
+            line.style.rotate = "0deg";
+            line.style.height = "90%";
+            break;
+
+        case 8:
+            line.style.rotate = "90deg";
+            line.style.height = "90%";
+    }
+}
+
+function endGame(){
+    cells.forEach(cell => {
+        cell.removeEventListener("click", handleClick);
+    });
 }
